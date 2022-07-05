4d15fabd0e163eef48eb69f0b361e658fe2cff9e
anton2920 <anton2920@gmail.com>
2022-07-05 16:02:15 +0300

    Addec copyright headers

--- src/libs/card.hpp
+++ src/libs/card.hpp
@@ -1,3 +1,23 @@
+/*
+   FreeCell — a free Windows freecell clone written on C++ with SFML
+   Copyright © Pavlovsky Anton, 2019-2022
+
+   This file is part of FreeCell.
+
+   FreeCell is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FreeCell is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FreeCell. If not, see <https://www.gnu.org/licenses/>.
+   */
+
 #ifndef SRC_CARD_HPP
 #define SRC_CARD_HPP
 
